be72f56ee8f3119e2afcee0150e788eedb7c48c3
secretwebmaster <earnbyshare2016@gmail.com>
2024-09-13 16:15:35 +0800

    Update backend footer year to be dynamically updated

--- resources/views/backend/parts/footer.blade.php
+++ resources/views/backend/parts/footer.blade.php
@@ -2,7 +2,7 @@
 <div id="kt_app_footer" class="app-footer">
 	<div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
 		<div class="text-dark order-2 order-md-1">
-			<span class="text-muted fw-semibold me-1">2023 &copy; @lang('word.all_rights_reserved')</span>
+			<span class="text-muted fw-semibold me-1">{{ date('Y') }} &copy; @lang('word.all_rights_reserved')</span>
 			<a href="https://3dayseo.com" target="_blank" class="text-gray-800 text-hover-primary">{{ gss('site_name') }}</a>
 		</div>
 
